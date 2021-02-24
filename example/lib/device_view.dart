7eb2a1a3a98cbc83283e1024d9653083b2c65720
Danny Albuquerque <danny.albuquerque@becare.swiss>
2021-02-24 17:02:35 +0100

    fix example app

---
+++ example/lib/device_view.dart
@@ -0,0 +1,280 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:polar_ble_sdk/polar_ble_sdk.dart';
+
+class DeviceView extends StatefulWidget {
+  final PolarDeviceInfo device;
+  final PolarBleSdk polarBleSdk;
+
+  DeviceView({
+    Key key,
+    @required this.device,
+    @required this.polarBleSdk,
+  }) : super(key: key);
+
+  @override
+  _DeviceViewState createState() => _DeviceViewState();
+}
+
+class _DeviceViewState extends State<DeviceView>
+    with AutomaticKeepAliveClientMixin<DeviceView> {
+  PolarBleSdk polarBleSdk;
+  AccelerometerData _lastAccData;
+  HrData _lastHrData;
+  EcgData _lastEcgData;
+  PpgData _lastPpgData;
+  bool connected = false;
+
+  StreamSubscription accSubscription;
+  StreamSubscription hrSubscription;
+  StreamSubscription ecgSubscription;
+  StreamSubscription ppgSubscription;
+
+  @override
+  void initState() {
+    polarBleSdk = widget.polarBleSdk;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String deviceId = widget.device.deviceId;
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Device name: ${widget.device.name}'),
+            SizedBox(height: 16),
+            RaisedButton(
+              onPressed: !connected ? () => connect(deviceId) : null,
+              child: Text('Connect'),
+            ),
+            RaisedButton(
+              onPressed: connected
+                  ? () => disconnect(deviceId)
+                  : () => disconnect(deviceId),
+              child: Text('Disconnect'),
+            ),
+            // RaisedButton(
+            //   onPressed: autoconnect,
+            //   child: Text('Autoconnect'),
+            // ),
+            //RaisedButton(onPressed: hrBroadcast, child: Text('HR broadcast'),),
+            SizedBox(height: 16),
+            RaisedButton(
+              onPressed: connected ? () => hr(deviceId) : null,
+              child: Text('HR'),
+            ),
+            RaisedButton(
+              onPressed: connected ? () => acc(deviceId) : null,
+              child: Text('ACC'),
+            ),
+            RaisedButton(
+              onPressed: connected ? () => ecg(deviceId) : null,
+              child: Text('ECG'),
+            ),
+            RaisedButton(
+              onPressed: connected ? () => ppg(deviceId) : null,
+              child: Text('PPG'),
+            ),
+            SizedBox(height: 32),
+            _lastAccData != null
+                ? Text('$deviceId: ${_lastAccData.toString()}')
+                : Container(),
+            _lastHrData != null
+                ? Text('$deviceId: ${_lastHrData.toString()}')
+                : Container(),
+            _lastEcgData != null
+                ? Text('$deviceId: ${_lastEcgData.toString()}')
+                : Container(),
+            _lastPpgData != null
+                ? Text('$deviceId: ${_lastPpgData.toString()}')
+                : Container(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void connect(String deviceId) async {
+    try {
+      await polarBleSdk.connect(deviceId);
+      setState(() {
+        connected = true;
+      });
+      print('connected to $deviceId');
+    } catch (e, stack) {
+      print(stack.toString());
+    }
+  }
+
+  void disconnect(String deviceId) async {
+    try {
+      hrSubscription?.cancel();
+      accSubscription?.cancel();
+      ecgSubscription?.cancel();
+      ppgSubscription?.cancel();
+      hrSubscription = null;
+      accSubscription = null;
+      ecgSubscription = null;
+      ppgSubscription = null;
+      setState(() {
+        _lastHrData = null;
+        _lastAccData = null;
+        _lastEcgData = null;
+        _lastPpgData = null;
+      });
+      await polarBleSdk.disconnect(deviceId);
+      print('disconnected of $deviceId');
+      setState(() {
+        connected = false;
+      });
+    } catch (e, stack) {
+      print(stack.toString());
+    }
+  }
+
+  // void autoconnect() async {
+  //   try {
+  //     await polarBleSdk.autoconnect();
+  //   } catch (e, stack) {
+  //     print(stack.toString());
+  //   }
+  // }
+
+  // void hrBroadcast() {
+  //   try {
+  //     polarBleSdk.hrBroadcast().listen(
+  //       (hr) {
+  //         print(hr);
+  //         setState(() {
+  //           _lastHr = hr;
+  //         });
+  //       },
+  //       onError: (e) {
+  //         print(e);
+  //       },
+  //       onDone: () => print('done'),
+  //       cancelOnError: true,
+  //     );
+  //   } catch (e, stack) {
+  //     print(stack.toString());
+  //   }
+  // }
+
+  void acc(String deviceId) {
+    if (accSubscription != null) {
+      accSubscription.cancel();
+      accSubscription = null;
+      setState(() {
+        _lastAccData = null;
+      });
+    } else {
+      try {
+        accSubscription = polarBleSdk.acc(deviceId).listen(
+          (accData) {
+            //print(accData.toString());
+            setState(() {
+              _lastAccData = accData;
+            });
+          },
+          onError: (e) => print(e),
+          onDone: () => print('done'),
+          cancelOnError: true,
+        );
+      } catch (e, stack) {
+        print(stack.toString());
+      }
+    }
+  }
+
+  void hr(String deviceId) {
+    if (hrSubscription != null) {
+      hrSubscription.cancel();
+      hrSubscription = null;
+      setState(() {
+        _lastHrData = null;
+      });
+    } else {
+      try {
+        hrSubscription = polarBleSdk.hr(deviceId).listen(
+          (hrData) {
+            //print(hrData.toString());
+            setState(() {
+              _lastHrData = hrData;
+            });
+          },
+          onError: (e) {
+            print(e);
+          },
+          onDone: () => print('done'),
+          cancelOnError: true,
+        );
+      } catch (e, stack) {
+        print(stack.toString());
+      }
+    }
+  }
+
+  void ecg(String deviceId) {
+    if (ecgSubscription != null) {
+      ecgSubscription.cancel();
+      ecgSubscription = null;
+      setState(() {
+        _lastEcgData = null;
+      });
+    } else {
+      try {
+        ecgSubscription = polarBleSdk.ecg(deviceId).listen(
+          (ecgData) {
+            //print(ecgData.toString());
+            setState(() {
+              _lastEcgData = ecgData;
+            });
+          },
+          onError: (e) {
+            print(e);
+          },
+          onDone: () => print('done'),
+          cancelOnError: true,
+        );
+      } catch (e, stack) {
+        print(stack.toString());
+      }
+    }
+  }
+
+  void ppg(String deviceId) {
+    if (ppgSubscription != null) {
+      ppgSubscription.cancel();
+      ppgSubscription = null;
+      setState(() {
+        _lastPpgData = null;
+      });
+    } else {
+      try {
+        ppgSubscription = polarBleSdk.ppg(deviceId).listen(
+          (ppgData) {
+            //print(ppgData.toString());
+            setState(() {
+              _lastPpgData = ppgData;
+            });
+          },
+          onError: (e) {
+            print(e);
+          },
+          onDone: () => print('done'),
+          cancelOnError: true,
+        );
+      } catch (e, stack) {
+        print(stack.toString());
+      }
+    }
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:polar_ble_sdk/polar_ble_sdk.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:polar_ble_sdk_example/device_view.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,23 +17,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  TextEditingController deviceIdCtrl1 = TextEditingController(text: '370C0628');
-  TextEditingController deviceIdCtrl2 = TextEditingController(text: '7F01D527');
   PolarBleSdk polarBleSdk = PolarBleSdk();
   PermissionStatus _locationPermissionStatus = PermissionStatus.unknown;
-  Map<String, AccelerometerData> _lastAccData = Map();
-  Map<String, HrData> _lastHrData = Map();
-  Map<String, EcgData> _lastEcgData = Map();
-  Map<String, PpgData> _lastPpgData = Map();
-  Map<String, bool> connectedDevices = Map();
-
-  Map<String, StreamSubscription> accSubscriptions = Map();
-  Map<String, StreamSubscription> hrSubscriptions = Map();
-  Map<String, StreamSubscription> ecgSubscriptions = Map();
-  Map<String, StreamSubscription> ppgSubscriptions = Map();
 
   StreamSubscription searchSubscription;
   List<PolarDeviceInfo> devices = [];
+  List<PolarDeviceInfo> selectedDevices = [];
   bool searchOnlyPolar = true;
 
   @override
@@ -46,14 +35,13 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: DefaultTabController(
-        length: 3,
+        length: selectedDevices.length + 1,
         child: Scaffold(
           appBar: AppBar(
             bottom: TabBar(
               tabs: [
                 Tab(text: "Search"),
-                Tab(text: "Device 1"),
-                Tab(text: "Device 2"),
+                for (var device in selectedDevices) Tab(text: device.name)
               ],
             ),
             title: const Text('PolarBleSdk example app'),
@@ -63,8 +51,8 @@ class _MyAppState extends State<MyApp> {
               ? TabBarView(
                   children: [
                     _buildSearchView(),
-                    _buildDevice(deviceIdCtrl1),
-                    _buildDevice(deviceIdCtrl2),
+                    for (var device in selectedDevices)
+                      DeviceView(device: device, polarBleSdk: polarBleSdk)
                   ],
                 )
               : Center(child: Text('Location permission needed')),
@@ -108,8 +96,15 @@ class _MyAppState extends State<MyApp> {
                 return Container();
               return ListTile(
                 onTap: () {
-                  deviceIdCtrl1.text = device.deviceId;
+                  setState(() {
+                    if (selectedDevices.contains(device)) {
+                      selectedDevices.remove(device);
+                    } else {
+                      selectedDevices.add(device);
+                    }
+                  });
                 },
+                selected: selectedDevices.contains(device),
                 title:
                     Text("${device.name.isNotEmpty ? device.name : 'unknown'}"),
                 subtitle: Text("${device.address}"),
@@ -137,106 +132,6 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-  Widget _buildDevice(TextEditingController controller) {
-    String deviceId = controller.text;
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              children: [
-                Text('Device ID: '),
-                SizedBox(width: 8),
-                Expanded(child: TextField(controller: controller)),
-              ],
-            ),
-            Center(
-              child: Row(
-                children: [
-                  RaisedButton(
-                    onPressed: () {
-                      setState(() {
-                        controller.text = '7F01D527';
-                      });
-                    },
-                    child: Text('OH1'),
-                  ),
-                  SizedBox(width: 16),
-                  RaisedButton(
-                    onPressed: () {
-                      setState(() {
-                        controller.text = '370C0628';
-                      });
-                    },
-                    child: Text('H10'),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 16),
-            RaisedButton(
-              onPressed: !connectedDevices.containsKey(deviceId)
-                  ? () => connect(deviceId)
-                  : null,
-              child: Text('Connect'),
-            ),
-            RaisedButton(
-              onPressed: connectedDevices.containsKey(deviceId)
-                  ? () => disconnect(deviceId)
-                  : null,
-              child: Text('Disconnect'),
-            ),
-            // RaisedButton(
-            //   onPressed: autoconnect,
-            //   child: Text('Autoconnect'),
-            // ),
-            //RaisedButton(onPressed: hrBroadcast, child: Text('HR broadcast'),),
-            SizedBox(height: 16),
-            RaisedButton(
-              onPressed: connectedDevices.containsKey(deviceId)
-                  ? () => hr(deviceId)
-                  : null,
-              child: Text('HR'),
-            ),
-            RaisedButton(
-              onPressed: connectedDevices.containsKey(deviceId)
-                  ? () => acc(deviceId)
-                  : null,
-              child: Text('ACC'),
-            ),
-            RaisedButton(
-              onPressed: connectedDevices.containsKey(deviceId)
-                  ? () => ecg(deviceId)
-                  : null,
-              child: Text('ECG'),
-            ),
-            RaisedButton(
-              onPressed: connectedDevices.containsKey(deviceId)
-                  ? () => ppg(deviceId)
-                  : null,
-              child: Text('PPG'),
-            ),
-            SizedBox(height: 32),
-            _lastAccData.containsKey(deviceId)
-                ? Text('$deviceId: ${_lastAccData[deviceId].toString()}')
-                : Container(),
-            _lastHrData.containsKey(deviceId)
-                ? Text('$deviceId: ${_lastHrData[deviceId].toString()}')
-                : Container(),
-            _lastEcgData.containsKey(deviceId)
-                ? Text('$deviceId: ${_lastEcgData[deviceId].toString()}')
-                : Container(),
-            _lastPpgData.containsKey(deviceId)
-                ? Text('$deviceId: ${_lastPpgData[deviceId].toString()}')
-                : Container(),
-          ],
-        ),
-      ),
-    );
-  }
-
   Future<void> _checkPermissions() async {
     if (Platform.isAndroid) {
       var permissionStatus = await PermissionHandler()
@@ -248,182 +143,6 @@ class _MyAppState extends State<MyApp> {
 
       if (_locationPermissionStatus != PermissionStatus.granted) {
         return Future.error(Exception("Location permission not granted"));
-      }
-    }
-  }
-
-  void connect(String deviceId) async {
-    try {
-      await polarBleSdk.connect(deviceId);
-      setState(() {
-        connectedDevices[deviceId] = true;
-      });
-      print('connected to $deviceId');
-    } catch (e, stack) {
-      print(stack.toString());
-    }
-  }
-
-  void disconnect(String deviceId) async {
-    try {
-      hrSubscriptions[deviceId]?.cancel();
-      accSubscriptions[deviceId]?.cancel();
-      ecgSubscriptions[deviceId]?.cancel();
-      ppgSubscriptions[deviceId]?.cancel();
-      hrSubscriptions.remove(deviceId);
-      accSubscriptions.remove(deviceId);
-      ecgSubscriptions.remove(deviceId);
-      ppgSubscriptions.remove(deviceId);
-      setState(() {
-        _lastHrData.remove(deviceId);
-        _lastAccData.remove(deviceId);
-        _lastEcgData.remove(deviceId);
-        _lastPpgData.remove(deviceId);
-      });
-      await polarBleSdk.disconnect(deviceId);
-      print('disconnected of $deviceId');
-      setState(() {
-        connectedDevices.remove(deviceId);
-      });
-    } catch (e, stack) {
-      print(stack.toString());
-    }
-  }
-
-  // void autoconnect() async {
-  //   try {
-  //     await polarBleSdk.autoconnect();
-  //   } catch (e, stack) {
-  //     print(stack.toString());
-  //   }
-  // }
-
-  // void hrBroadcast() {
-  //   try {
-  //     polarBleSdk.hrBroadcast().listen(
-  //       (hr) {
-  //         print(hr);
-  //         setState(() {
-  //           _lastHr = hr;
-  //         });
-  //       },
-  //       onError: (e) {
-  //         print(e);
-  //       },
-  //       onDone: () => print('done'),
-  //       cancelOnError: true,
-  //     );
-  //   } catch (e, stack) {
-  //     print(stack.toString());
-  //   }
-  // }
-
-  void acc(String deviceId) {
-    if (accSubscriptions[deviceId] != null) {
-      accSubscriptions[deviceId].cancel();
-      accSubscriptions[deviceId] = null;
-      setState(() {
-        _lastAccData.remove(deviceId);
-      });
-    } else {
-      try {
-        accSubscriptions[deviceId] = polarBleSdk.acc(deviceId).listen(
-          (accData) {
-            print(accData.toString());
-            setState(() {
-              _lastAccData[deviceId] = accData;
-            });
-          },
-          onError: (e) => print(e),
-          onDone: () => print('done'),
-          cancelOnError: true,
-        );
-      } catch (e, stack) {
-        print(stack.toString());
-      }
-    }
-  }
-
-  void hr(String deviceId) {
-    if (hrSubscriptions[deviceId] != null) {
-      hrSubscriptions[deviceId].cancel();
-      hrSubscriptions.remove(deviceId);
-      setState(() {
-        _lastHrData.remove(deviceId);
-      });
-    } else {
-      try {
-        hrSubscriptions[deviceId] = polarBleSdk.hr(deviceId).listen(
-          (hrData) {
-            print(hrData.toString());
-            setState(() {
-              _lastHrData[deviceId] = hrData;
-            });
-          },
-          onError: (e) {
-            print(e);
-          },
-          onDone: () => print('done'),
-          cancelOnError: true,
-        );
-      } catch (e, stack) {
-        print(stack.toString());
-      }
-    }
-  }
-
-  void ecg(String deviceId) {
-    if (ecgSubscriptions[deviceId] != null) {
-      ecgSubscriptions[deviceId].cancel();
-      ecgSubscriptions[deviceId] = null;
-      setState(() {
-        _lastEcgData.remove(deviceId);
-      });
-    } else {
-      try {
-        ecgSubscriptions[deviceId] = polarBleSdk.ecg(deviceId).listen(
-          (ecgData) {
-            print(ecgData.toString());
-            setState(() {
-              _lastEcgData[deviceId] = ecgData;
-            });
-          },
-          onError: (e) {
-            print(e);
-          },
-          onDone: () => print('done'),
-          cancelOnError: true,
-        );
-      } catch (e, stack) {
-        print(stack.toString());
-      }
-    }
-  }
-
-  void ppg(String deviceId) {
-    if (ppgSubscriptions[deviceId] != null) {
-      ppgSubscriptions[deviceId].cancel();
-      ppgSubscriptions[deviceId] = null;
-      setState(() {
-        _lastPpgData.remove(deviceId);
-      });
-    } else {
-      try {
-        ppgSubscriptions[deviceId] = polarBleSdk.ppg(deviceId).listen(
-          (ppgData) {
-            print(ppgData.toString());
-            setState(() {
-              _lastPpgData[deviceId] = ppgData;
-            });
-          },
-          onError: (e) {
-            print(e);
-          },
-          onDone: () => print('done'),
-          cancelOnError: true,
-        );
-      } catch (e, stack) {
-        print(stack.toString());
       }
     }
   }
